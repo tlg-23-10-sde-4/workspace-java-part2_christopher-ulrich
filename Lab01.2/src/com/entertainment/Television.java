7392f4bc0fa9b1a1065d34150838a099736ffa28
Christopher <christopher.ulrich@tlgcohort.com>
2023-10-31 17:50:38 -0700

    Lab01.3 (equals/hashCode) complete

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -4,6 +4,8 @@
 
 package com.entertainment;
 
+import java.util.Objects;
+
 public class Television { // Private or Public or public/private (default)?
 
     // CLASS (static or public) variables - these are shared among all instances
@@ -47,6 +49,34 @@ public class Television { // Private or Public or public/private (default)?
 
     public void setVolume(int volume) {
         this.volume = volume;
+    }
+
+    @Override
+    public int hashCode() {
+        /* This is a poorly written hash function because it easily yields "hash collisions."
+         * A hash collision is when "different" objects have the same hash code (just by coincidence).
+         */
+        // return getBrand().length() + getVolume();
+
+        // Instead we rely on Objects.hash() to give us a valid hash function
+        return Objects.hash(getBrand(), getVolume());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        // Proceed ONLY IF obj is referencing a Television object
+        if (obj instanceof Television) {
+            // Downcast obj reference to more specific Person type
+            Television other = (Television) obj;
+
+            // Do the checks: (ignoring case if name is null...bad)
+            // Is my name the same as the other one's name? &&
+            // Is my age the same, too?
+            result = Objects.equals(this.getBrand(), other.getBrand()) &&   // Null safe check
+                    this.getVolume() == other.getVolume();  // Primitives cannot be null
+        }
+        return result;
     }
 
     // public String toString() {}  // For output
--- Lab01.2/src/com/entertainment/client/TelevisionClient.java
+++ Lab01.2/src/com/entertainment/client/TelevisionClient.java
@@ -6,9 +6,12 @@ package com.entertainment.client;
 
 import com.entertainment.Television;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class TelevisionClient {
     public static void main(String[] args) {
-        Television tv1 = new Television();
+/*        Television tv1 = new Television();
         Television tv2 = new Television("Samsung", 12);
 
         System.out.println(tv1);
@@ -16,13 +19,23 @@ class TelevisionClient {
 
         tv2.changeChannel(42);  // toString() automatically called
         System.out.println(tv2);
-        System.out.println();   // Blank line
+        System.out.println();   // Blank line*/
 
         Television tvA = new Television("Sony", 50);
         Television tvB = new Television("Sony", 50);
 
         System.out.println("tvA == tvB: " + (tvA == tvB));
         System.out.println("tvA.equals(tvB): " + tvA.equals(tvB));
+        System.out.println();
+
+//        System.out.println(tvA.hashCode());
+//        System.out.println(tvB.hashCode());
+//        System.out.println();
+
+        Set<Television> tvs = new HashSet<>();
+        tvs.add(tvA);
+        tvs.add(tvB);   // Should be rejected as a duplicate
+        System.out.println("The size of the set is: " + tvs.size());
     }
 
     /**
