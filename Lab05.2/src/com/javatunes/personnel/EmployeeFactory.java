0a63c3f136d8c2528f5a06f57fc8c3894a9e673f
Christopher <christopher.ulrich@tlgcohort.com>
2023-11-06 14:44:33 -0800

    Lab05.2 Maps & Test First

--- Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
+++ Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
@@ -12,20 +12,47 @@ import java.sql.Date;
 import java.util.Map;
 
 public class EmployeeFactory {
-  
-  // prevent direct instantiation - this is an all-static factory class
-  private EmployeeFactory() {
-  }
-  
-  /**
-   * TODO: given the input map, create and return the correct object (with its properties set).
-   * If the input map's "type" value is not "HE" or "SE", throw IllegalArgumentException with a suitable message.
-   */
-  public static Employee createEmployee(Map<String,String> inputMap)
-  throws IllegalArgumentException {
-    // return value
-    Employee emp = null;
-    
-    return emp;
-  }
+
+    // prevent direct instantiation - this is an all-static factory class
+    private EmployeeFactory() {
+    }
+
+    /**
+     * TODO: given the input map, create and return the correct object (with its properties set).
+     * If the input map's "type" value is not "HE" or "SE", throw IllegalArgumentException with a suitable message.
+     */
+    // Creates a reference to an abstract class
+    public static Employee createEmployee(Map<String, String> inputMap) throws IllegalArgumentException {
+        // return value
+        Employee emp = null;
+
+        // read the 'indicator' out of the Map
+        String type = inputMap.get("type");
+
+        if (!"SE".equals(type) && !"HE".equals(type)) {
+            throw new IllegalArgumentException("Invalid type: " + type + ". Must be either SE or HE.");
+        }
+
+        String name     = inputMap.get("name"); // can throw these up here to prevent redundancies below
+        Date hireDate   = Date.valueOf(inputMap.get("hireDate"));
+
+        if ("SE".equals(type)) {
+//            String name   = inputMap.get("name");
+//            Date hireDate = Date.valueOf(inputMap.get("hireDate"));
+            Double salary = Double.valueOf(inputMap.get("salary"));
+            emp = new SalariedEmployee(name, hireDate, salary);
+        }
+        else if ("HE".equals(type)) {
+            // TODO
+//            String name = inputMap.get("name");
+//            Date hireDate = Date.valueOf(inputMap.get("hireDate"));
+            Double rate = Double.valueOf(inputMap.get("rate"));
+            Double hours = Double.valueOf(inputMap.get("hours"));
+            emp = new HourlyEmployee(name, hireDate, rate, hours);
+        }
+//        else {    // Less efficient way
+//            throw new IllegalArgumentException("Invalid type: " + type + ". Must be either SE or HE.");
+//        }
+        return emp;
+    }
 }
